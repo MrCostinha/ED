52bf75f799b7069e3b9be6f58eaded595d7be5eb
Matheus Costa <matheuscostamc135@gmail.com>
2023-08-31 15:23:14 -0300

    Aula 31/08

--- Lista 0/8.c
+++ Lista 0/8.c
@@ -11,17 +11,11 @@ typedef struct aluno Aluno;
 int main() {
     Aluno si[3];
 
-    strcpy(si[0].matricula, "5552890");
-    strcpy(si[0].nome, "Matheus Costa");
-    si[0].nota = 10.0;
-
-    strcpy(si[1].matricula, "1234567");
-    strcpy(si[1].nome, "Maria Rita");
-    si[1].nota = 8.75;
-
-    strcpy(si[2].matricula, "7654321");
-    strcpy(si[2].nome, "Obede Lima");
-    si[2].nota = 9.5;
+    for(int i = 0; i < 3; i++) {
+        scanf("%[^/]", &si[i].matricula);
+        scanf("%[^/]", &si[i].nome);
+        scanf("%f", &si[i].nota);
+    }
 
     for (int i = 0; i < 3; i++) {
         printf("%s - %s - %.2f\n", si[i].matricula, si[i].nome, si[i].nota);
